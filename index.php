3736dee4e82afe8106da3e2512f27190f186ab0a
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.attlocal.net>
2020-11-13 00:57:12 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <title><?php echo "Ask The Genie"; ?></title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+        <div class="container">
+            <a class="navbar-brand" href="#">Ask The Genie</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Previous Questions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Search</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="card card-body bg-light mt-5">
+            <h2>Ask A Question</h2>
+            <p>Type your question & press 'Submit'</p>
+            <?php
+            if ($_POST["submit"]) {
+                if (empty($_POST['question'])) { ?>
+                    <div class="alert alert-danger" role="alert"><?php $questionErr = "Question is required"; ?>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <?php
+                        echo $questionErr; ?></div><?php
+                                            }
+                                        }
+                                                ?>
+            <form action="index.php" method="post">
+                <div class="form-group">
+                    <label for="body">What would you like to ask?</label>
+                    <textarea name="question" class="form-control form-control-lg"></textarea>
+                </div>
+                <input type="submit" name="submit" class="btn btn-success" value="Submit">
+            </form>
+        </div>
+        <div class="mt-5">
+
+            <?php
+            if ($_POST["submit"]) {
+                if (!empty($_POST['question'])) {
+
+                    echo "<div class='alert alert-success' role='alert'>
+                <h4 class='alert-heading'>Hmmm Let Me Think!</h4>
+                <p>Your chances look good!</p>
+                <hr>
+                <p class='mb-0'>- The Genie</p>
+                </div>";
+                }
+            }
+            ?>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+
+</html>
